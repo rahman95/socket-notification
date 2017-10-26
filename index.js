47c31d4e76139638aee0c588c189553300221dc5
Rahman Younus <rahman_95@live.co.uk>
2017-10-26 21:05:07 +0100

    got app listening to socket

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+app = express()
+server = require('http').Server(app);
+io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
